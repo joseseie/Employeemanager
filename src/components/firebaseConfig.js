8aada8dc04ed1007e1eb3b06aece4c19e08175d7
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-24 20:02:23 +0200

    Added firebase configurations

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBgFKdCQy3sScNpVVvtKPjyO6SQFQ4zrMc",
+    authDomain: "vue-project-7415b.firebaseapp.com",
+    databaseURL: "https://vue-project-7415b.firebaseio.com",
+    projectId: "vue-project-7415b",
+    storageBucket: "vue-project-7415b.appspot.com",
+    messagingSenderId: "927557502296"
+}
